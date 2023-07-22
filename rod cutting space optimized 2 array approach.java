10498c6fb6f3ae8153fdbe01c954f83269175614
Abir Dey <116991542+AbirDey2002@users.noreply.github.com>
2023-07-22 14:48:44 +0530

    Create rod cutting space optimized 2 array approach.java

---
+++ rod cutting space optimized 2 array approach.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.math.*;
+
+public class Solution {
+	public static int cutRod(int price[], int n) {
+		// Write your code here.
+
+		int[] prev = new int[n+1]; int[] curr = new int[n+1];
+
+		for(int i = 0;i<n+1;i++){
+			prev[i] = i*price[0];
+		}
+
+		for(int ind = 1;ind<n;ind++){
+			for(int N = 0;N<=n;N++){
+				int rod_len = ind+1;
+				int not_take = prev[N];
+				int take = (int) -1e9;
+				if(rod_len <= N) take = curr[N-rod_len] + price[ind];
+				curr[N] = Math.max(take,not_take);
+			}
+			prev = curr;
+		}
+
+		return prev[n];
+	}
+}
